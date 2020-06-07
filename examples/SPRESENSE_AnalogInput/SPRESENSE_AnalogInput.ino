7015899a4321bbf9c0e03c9096c0fbdee2e069d4
ThousanDIY <tomorrow56@gmail.com>
2020-06-07 13:40:42 +0900

    Input error correction function added

--- examples/SPRESENSE_AnalogInput/SPRESENSE_AnalogInput.ino
+++ examples/SPRESENSE_AnalogInput/SPRESENSE_AnalogInput.ino
@@ -1,35 +1,15 @@
 /*
-  Analog Input
-
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInput
+  Analog Input test for SPRESENSE
 */
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+uint16_t AIN4;
+uint16_t AIN5;
+
+uint16_t AIN4_MAX = 1004; // ADC Value when 3.3V input
+uint16_t AIN5_MAX = 990; // ADC Value when 3.3V input
+
+float AIN4_V;
+float AIN5_V;
 
 void setup() {
     Serial.begin(115200);
@@ -40,7 +20,11 @@ void setup() {
 }
 
 void loop() {
-    Serial.println("AIN4=" + String(analogRead(A2)));
+    AIN4 = analogRead(A2);
+    AIN4_V = 3.3 * ((float)AIN4/AIN4_MAX);
+
+    Serial.println("AIN4 =" + String(AIN4));
+    Serial.println("AIN4 =" + String(AIN4_V) + " V");
     digitalWrite(LED0, HIGH);
     delay(100);
     digitalWrite(LED1, HIGH);
@@ -50,7 +34,11 @@ void loop() {
     digitalWrite(LED3, HIGH);
     delay(300);
 
-    Serial.println("AIN5=" + String(analogRead(A3)));
+    AIN5 = analogRead(A3);
+    AIN5_V = 3.3 * ((float)AIN5/AIN5_MAX);
+
+    Serial.println("AIN5=" + String(AIN5));
+    Serial.println("AIN5 =" + String(AIN5_V) + " V");
     digitalWrite(LED0, LOW);
     delay(100);
     digitalWrite(LED1, LOW);
